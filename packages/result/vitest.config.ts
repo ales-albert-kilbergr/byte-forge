86f2c9c5749518dc802220d877e2c0c4ecc7ca7a
Ales Albert KILBERGR <ales.albert@kilbergr.eu>
2025-09-02 08:17:34 +0200

    test(result): require full coverage, because this is a small package with already stable interface

--- packages/result/vitest.config.ts
+++ packages/result/vitest.config.ts
@@ -20,10 +20,10 @@ export default defineConfig({
       ],
       thresholds: {
         global: {
-          statements: 80,
-          branches: 80,
-          functions: 80,
-          lines: 80,
+          statements: 100,
+          branches: 100,
+          functions: 100,
+          lines: 100,
         },
       },
     },
